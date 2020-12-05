fa625bd91410d356368c895ca74262faefd0c088
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2020-12-05 20:31:05 +0000

    add nicer way

--- day05/day05_nicer_way.R
+++ day05/day05_nicer_way.R
@@ -1,3 +1,5 @@
+# h/t riinu pius
+
 dat <- read.table("day05/input", col.names = "encoded")
 dat$encoded <- gsub("F|L", "0", dat$encoded)
 dat$encoded <- gsub("B|R", "1", dat$encoded)
@@ -7,7 +9,5 @@ dat$seat_ids <- strtoi(dat$encoded, base = 2)
 max(dat$seat_ids)
 
 # part two ---------------------------------------------------------------------
-dat <- dat[order(dat$seat_ids), ]
-dat$diff <- c(NA, diff(dat$seat_ids, 1))
-dat <- na.omit(dat)
-dat$seat_ids[dat$diff == 2 ] - 1
+possible_seats <- min(dat$seat_ids):max(dat$seat_ids) 
+possible_seats[!possible_seats %in% dat$seat_ids]
