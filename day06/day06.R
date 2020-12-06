d50d96e1fd562f686c9928b4b4cb6f133ab65c34
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2020-12-06 16:45:22 +0000

    cleanup

--- day06/day06.R
+++ day06/day06.R
@@ -1,8 +1,12 @@
 input <- readLines("day06/input")
-dat <- tapply(input, cumsum(input == ""), paste, collapse = " ")
-counts <- sapply(strsplit(dat, ""), function(x) length(unique(x)) - 1)
+
+# part one ---------------------------------------------------------------------
+dat <- tapply(input, cumsum(input == ""), paste, collapse = "")
+counts <- sapply(strsplit(dat, ""), function(x) length(unique(x)))
 sum(counts)
 
+# part two ---------------------------------------------------------------------
+dat <- tapply(input, cumsum(input == ""), paste, collapse = " ")
 tmp <- lapply(sapply(dat, strsplit, " "), function(x) x[x != ""])
 tmp <- lapply(tmp, function(x) Reduce(intersect, strsplit(x, "")))
-tmp <- sum(sapply(tmp, length))
+sum(sapply(tmp, length))
